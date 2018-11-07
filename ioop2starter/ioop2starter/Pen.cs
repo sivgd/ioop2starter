7316e4b84b5da015db3619e896cd28a18f0048ed
Bassett James B <BassettJB@mukilteo.wednet.edu>
2018-11-07 13:19:32 -0800

    Initial Push

---
+++ ioop2starter/ioop2starter/Pen.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ioop2starter
+{
+    class Pen
+    {
+        //Properties
+        public string Name;
+        public string Color;
+        public float Price;
+        public uint PercentInkRemaining;
+        public bool IsEmpty;
+
+
+        //Constructors
+        public Pen()
+        {
+
+        }
+
+        public Pen(string name, string color, float price, uint percentInkRemaining, bool isEmpty)
+        {
+            Name = name;
+            Color = color;
+            Price = price;
+            PercentInkRemaining = percentInkRemaining;
+            IsEmpty = isEmpty;
+        }
+
+
+        //Methods
+        public void WriteName()
+        {
+            Console.WriteLine("My name is " + this.Name);
+        }
+
+        public void WriteColor()
+        {
+            Console.WriteLine("My color is " + this.Color);
+        }
+
+        public void WriteInkLevel()
+        {
+            Console.WriteLine("I have " + this.PercentInkRemaining + "% ink remaining.");
+        }
+
+        public void UseInk(uint percentToUse)
+        {
+            this.PercentInkRemaining -= percentToUse;
+        }
+
+        public void Refill()
+        {
+            this.PercentInkRemaining = 100;
+        }
+
+        public float GetCurrentMonetaryValue()
+        {
+            return (this.PercentInkRemaining / 100.0f * this.Price);
+        }
+    }
+}
---
+++ ioop2starter/ioop2starter/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ioop2starter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("I.OOP.0x02:::::");
+
+            //Create some pens...
+
+            Pen myCrystalBic = new Pen();
+            myCrystalBic.Name = "Crystal Bic";
+            myCrystalBic.Color = "RED";
+            myCrystalBic.Price = 0.50f;
+            myCrystalBic.PercentInkRemaining = 100;
+            myCrystalBic.IsEmpty = false;
+
+            Pen mySharpie = new Pen("Sharpie", "BLACK", 1.49f, 100, false);
+
+
+
+            //Try some methods...
+            mySharpie.WriteName();
+            mySharpie.WriteColor();
+            mySharpie.WriteInkLevel();
+            mySharpie.UseInk(10);
+            mySharpie.WriteInkLevel();
+            Console.WriteLine(mySharpie.GetCurrentMonetaryValue());
+            mySharpie.Refill();
+            mySharpie.WriteInkLevel();
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
